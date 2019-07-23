25e626aa5768e52a628257ff7e3cbccb8a1e7682
erka <erik.westerberg@hotmail.se>
2019-07-23 23:11:09 +0200

    fetching the api and grabbing certain info from it

---
+++ app.js
@@ -0,0 +1,36 @@
+
+
+window.addEventListener("load", () => {
+    let long;
+    let lat;
+    let temperatureDescription = document.querySelector(
+        ".temperature-description");
+    let temperatureDegree = document.querySelector(".temperature-degree");
+    let locationTimezon = document.querySelector(".location-timezon");
+
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+            
+            const proxy = `https://cors-anywhere.herokuapp.com/`;
+            const api = `${proxy}https://api.darksky.net/forecast/4de3d15b9c5d7a289ea5245786a8c1ba/${lat},${long}`;
+            
+    fetch(api)
+        .then(response => {
+            return response.json();
+        })
+        .catch(error => {
+            console.log(error);
+          })
+
+        .then(data => {
+            console.log(data);
+            const { temperature, summary } = data.currently;
+            //set DOM Elements from the API
+            temperatureDegree.textContent = temperature;
+        })
+     });
+    }
+});
